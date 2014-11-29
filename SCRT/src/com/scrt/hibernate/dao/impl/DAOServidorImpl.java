fdbb44158de8abbd58a9d10bccd3016d0f28a0c8
yamto.elsuperior <yamto.elsuperior@gmail.com>
2014-11-29 01:38:04 +0000

    Posee el archivo de pruebas unitarias solo con el metodo de listar servidores hay que poder hacer que corra con el JUnit

---
+++ SCRT/src/com/scrt/hibernate/dao/impl/DAOServidorImpl.java
@@ -0,0 +1,129 @@
+package com.scrt.hibernate.dao.impl;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import com.scrt.hibernate.model.Servidor;
+import com.sct.hibernate.dao.DAOServidor;
+
+
+public class DAOServidorImpl implements DAOServidor {
+// Los comentarios respectivos de cada metodo estan en el DAOServidor del paquete com.scrt.hibernate.dao
+	
+	private static Logger logger = Logger.getLogger(DAOServidorImpl.class);
+	
+	@SuppressWarnings("unchecked")
+	
+	@Override
+	public List<Servidor> ListarServidores() {
+		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		List<Servidor> encontradas = new ArrayList<Servidor>();
+		
+		try {
+			encontradas = session.createQuery("from Categoria").list();
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}
+		
+		return encontradas;
+	}
+
+	@Override
+	public Servidor RetornarServidor(int ID) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Servidor encontrada = null;
+		
+		try {
+			encontrada = (Servidor) session.get(Servidor.class,ID);
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}
+		
+		return encontrada;
+	}
+
+	@Override
+	public void guardarServidor(Servidor servidor) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		
+		try {
+			session.save(servidor);
+			session.getTransaction().commit();
+			
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}		
+	}
+
+	@Override
+	public void eliminarServidor(Servidor servidor) {
+	
+		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		
+		try {
+			session.delete(servidor);
+			session.getTransaction().commit();
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}
+	}
+
+	@Override
+	public void editarServidor(Servidor servidor) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		session.beginTransaction();
+		
+		try {
+			session.merge(servidor);
+			session.getTransaction().commit();
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}
+	}
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Servidor> buscarServidor(String modelo) {
+		
+		Session session = HibernateUtil.getSessionFactory().openSession(); 
+		Criteria criteria = session.createCriteria(Servidor.class);
+		List<Servidor> encontradas = new ArrayList<Servidor>();
+		
+		try {
+			criteria.add(Restrictions.like("nombre", "%"+modelo+"%"));
+			encontradas = criteria.list();
+		} catch (HibernateException ex) {
+			logger.debug("Exception encontrada" + ex.getLocalizedMessage());
+		} finally {
+			session.close();
+		}
+		
+		return encontradas;
+	}
+
+	
+	
+	
+	
+	
+	
+}
--- SCRT/src/com/sct/hibernate/dao/DAOServidor.java
+++ SCRT/src/com/sct/hibernate/dao/DAOServidor.java
@@ -1,5 +1,45 @@
 package com.sct.hibernate.dao;
 
-public class DAOServidor {
+import java.util.List;
 
+
+
+
+import com.scrt.hibernate.model.Servidor;
+
+
+    // Interfaz de acceso a los datos de la tabla Servidor
+
+public interface DAOServidor {
+
+	// Metodo  que retorna la lista de servidores que se encuentran la BD
+	public List<Servidor> ListarServidores();
+	
+	//Metodo que retorna una instancia del servidor atraves de un parametro identificador
+	
+	public Servidor RetornarServidor(int ID);
+	
+	
+	//Metodo que guarda una instancia de un servidor en la BD
+	public void guardarServidor(Servidor servidor);
+	
+	//Metodo que permite elminar una instancia de un servidor en la BD
+	public void eliminarServidor(Servidor servidor);
+	
+	//Metodo que permite editar una instacia existente de un servidor en la BD
+	
+	public void editarServidor(Servidor servidor);
+	
+	/*
+	 * Metodo que permite realizar una busqueda de una categoria a partir de una
+	 * palabra clave en su atributo modelo
+	 * @param cadena Keyword para realizar la busqueda
+	 * @return Listado de entidades de tipo Servidor que coinciden con el string de busqueda
+	 */
+	public List<Servidor> buscarServidor(String modelo);
+	
+	
+	
+	
+	
 }
