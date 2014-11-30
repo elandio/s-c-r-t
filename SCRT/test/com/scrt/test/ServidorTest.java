c3cdefb20a466a07b973fe414c301cbc312a22c3
yamto.elsuperior <yamto.elsuperior@gmail.com>
2014-11-30 22:52:47 +0000

    Clase de pruebas unitarias completa

--- SCRT/test/com/scrt/test/ServidorTest.java
+++ SCRT/test/com/scrt/test/ServidorTest.java
@@ -9,6 +9,8 @@ import com.scrt.hibernate.dao.impl.DAOServidorImpl;
 import com.scrt.hibernate.model.Servidor;
 
 public class ServidorTest {
+	
+	
 	public void ListarServidores() {
 		DAOServidorImpl daoServidor = new DAOServidorImpl();
 		
@@ -22,4 +24,55 @@ public class ServidorTest {
 					" Empresa Propietaria: "+serv.getEmpresaPropietaria()+"Estado:"+serv.getEstado());
 		}
 	}
+		
+	public void RetornarServidor() {
+			DAOServidorImpl daoServidor = new DAOServidorImpl();
+			Servidor serv = daoServidor.RetornarServidor(2);
+			
+			System.out.println(serv.getID());
+		}
+	
+	
+	
+	public void guardarServidor() {
+		DAOServidorImpl daoServidor = new DAOServidorImpl();
+		Servidor serv = new Servidor();
+		serv.setEmpresaPropietaria(265825);
+		serv.setEstado("OK");
+		serv.setMarca("lenovo");
+		serv.setModelo("xp68520");
+		serv.setID(355522252);
+		daoServidor.guardarServidor(serv);
+	}
+	
+	public void eliminarServidor() {
+		DAOServidorImpl daoServidor = new DAOServidorImpl();
+		Servidor serv  = daoServidor.RetornarServidor(3);
+		daoServidor.eliminarServidor(serv);
+	}
+
+	public void editarServidor() {
+		DAOServidorImpl daoServidor = new DAOServidorImpl();
+		Servidor serv  = daoServidor.RetornarServidor(3);
+		serv.setEstado("OFF");
+		daoServidor.editarServidor(serv);
+	}
+	
+	@Test
+	public void buscarServidor(){
+		DAOServidorImpl daoServidor = new DAOServidorImpl();
+		List<Servidor> servs = daoServidor.buscarServidor("3352");
+		
+		System.out.println("Encontrados " + servs.size());
+		for(Servidor serv : servs) {
+			System.out.println("ID:"+serv.getID()+
+					" Modelo: "+serv.getModelo() +
+					" Marca: "+serv.getMarca()+ 
+					" Empresa pripietaria:"+serv.getEmpresaPropietaria());
+		}
+	}
+	
+	
+	
+	
 }
